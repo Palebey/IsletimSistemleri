b2341e602491f9250359dbf7a83225ea0d146886
Palebey <120555403+Palebey@users.noreply.github.com>
2024-12-25 23:44:31 +0300

    main fonksiyon ve kütüphaneler

---
+++ isletimsisodev/main.h
@@ -0,0 +1,27 @@
+#ifndef PROGRAM_H
+#define PROGRAM_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <ctype.h>
+
+#define MAX_LINE 1024
+
+// Fonksiyon prototipleri
+void handle_cd(char* path);
+void handle_help();
+void handle_increment(int num);
+void handle_quit();
+void handle_echo(char *arg);
+void handle_sleep(int sec);
+void handle_ls();
+void handle_cat(char *filename);
+void execute_command(char* input);
+void handle_pipe_and_semicolon(char* input);
+
+#endif // PROGRAM_H
